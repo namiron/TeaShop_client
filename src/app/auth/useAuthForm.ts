a3706e3caf9060f197359c0f292bb8e0a2082405
namiron <sasha_zpua@mail.ru>
2024-12-08 15:35:37 +0200

    new_build

--- src/app/auth/useAuthForm.ts
+++ src/app/auth/useAuthForm.ts
@@ -1,13 +1,20 @@
-import { useMutation } from '@tanstack/react-query'
-import { useRouter } from 'next/navigation'
-import { SubmitHandler, useForm } from 'react-hook-form'
-import toast from 'react-hot-toast'
+import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 
-import { DASHBOARD_URL } from '@/config/url.config'
 
-import { authService } from '@/services/auth/auth.service'
 
-import { IAuthForm } from '@/shared/types/auth.interface'
+import { DASHBOARD_URL } from '@/config/url.config';
+
+
+
+import { authService } from '@/services/auth/auth.service';
+
+
+
+import { IAuthForm } from '@/shared/types/auth.interface';
+
 
 export function useAuthForm(isReg: boolean) {
 	const router = useRouter()
@@ -22,14 +29,14 @@ export function useAuthForm(isReg: boolean) {
 			authService.main(isReg ? 'register' : 'login', data),
 		onSuccess() {
 			form.reset()
-			toast.success('Успешная авторизация')
+			toast.success('Successful authorization')
 			router.replace(DASHBOARD_URL.home())
 		},
 		onError(error) {
 			if (error.message) {
 				toast.error(error.message)
 			} else {
-				toast.error('Ошибка при авторизации')
+				toast.error('Error while logging in')
 			}
 		}
 	})
